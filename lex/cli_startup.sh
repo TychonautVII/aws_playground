99104a096780e03f9cd3f4663cdd54b7f70382e7
Peter Mariani <PMariani@mathematica-mpr.com>
2018-03-21 07:55:17 -0400

    cleaned up, and added a section to store material for the cloud guru labs

--- lex/cli_startup.sh
+++ lex/cli_startup.sh
@@ -3,27 +3,22 @@
 aws iam create-service-linked-role --aws-service-name lex.amazonaws.com --profile peter-personal
 
 aws lex-models put-slot-type \
-    --region us-east-1 \
     --name FlowerTypes \
     --cli-input-json file://FlowerTypes.json --profile peter-personal
 
 aws lex-models put-intent \
-   --region us-east-1 \
    --name OrderFlowers \
    --cli-input-json file://OrderFlowers.json --profile peter-personal
 
 aws lex-models put-bot \
-    --region us-east-1 \
     --name OrderFlowersBot \
     --cli-input-json file://OrderFlowersBot.json --profile peter-personal
 
  aws lex-models get-bot \
-    --region us-east-1 \
     --name OrderFlowersBot \
     --version-or-alias "\$LATEST" --profile peter-personal
 
  aws lex-runtime post-text \
-    --region us-east-1 \
     --bot-name OrderFlowersBot \
     --bot-alias "\$LATEST" \
     --user-id UserOne \
