d71d1cc9ef327378f736a837afcb9aa0ec00de3c
Peter Mariani <peterbmariani@gmail.com>
2018-03-22 11:04:48 -0400

    updated CICD stuff'

--- CICD/launch_script.sh
+++ CICD/launch_script.sh
@@ -1,5 +1,7 @@
+# Update Linux
 sudo yum update -y
 
+# Install and Start Jenkins
 sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
 sudo yum install jenkins -y
